de392b7e2d32e94ec992d695ae15177d4c2c90b5
Philipp Tusch <philipp.tusch@huf-group.com>
2017-10-06 12:29:06 +0200

    Yop

---
+++ build/Release/.deps/Release/obj.target/hello/hello.o.d
@@ -0,0 +1,59 @@
+cmd_Release/obj.target/hello/hello.o := g++ '-DNODE_GYP_MODULE_NAME=hello' '-DUSING_UV_SHARED=1' '-DUSING_V8_SHARED=1' '-DV8_DEPRECATION_WARNINGS=1' '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-DBUILDING_NODE_EXTENSION' -I/home/phil/.node-gyp/8.6.0/include/node -I/home/phil/.node-gyp/8.6.0/src -I/home/phil/.node-gyp/8.6.0/deps/uv/include -I/home/phil/.node-gyp/8.6.0/deps/v8/include -I../node_modules/nan  -fPIC -pthread -Wall -Wextra -Wno-unused-parameter -m64 -O3 -fno-omit-frame-pointer -fno-rtti -fno-exceptions -std=gnu++0x -MMD -MF ./Release/.deps/Release/obj.target/hello/hello.o.d.raw   -c -o Release/obj.target/hello/hello.o ../hello.cc
+Release/obj.target/hello/hello.o: ../hello.cc ../node_modules/nan/nan.h \
+ /home/phil/.node-gyp/8.6.0/include/node/node_version.h \
+ /home/phil/.node-gyp/8.6.0/include/node/uv.h \
+ /home/phil/.node-gyp/8.6.0/include/node/uv-errno.h \
+ /home/phil/.node-gyp/8.6.0/include/node/uv-version.h \
+ /home/phil/.node-gyp/8.6.0/include/node/uv-unix.h \
+ /home/phil/.node-gyp/8.6.0/include/node/uv-threadpool.h \
+ /home/phil/.node-gyp/8.6.0/include/node/uv-linux.h \
+ /home/phil/.node-gyp/8.6.0/include/node/node.h \
+ /home/phil/.node-gyp/8.6.0/include/node/v8.h \
+ /home/phil/.node-gyp/8.6.0/include/node/v8-version.h \
+ /home/phil/.node-gyp/8.6.0/include/node/v8config.h \
+ /home/phil/.node-gyp/8.6.0/include/node/node_version.h \
+ /home/phil/.node-gyp/8.6.0/include/node/node_buffer.h \
+ /home/phil/.node-gyp/8.6.0/include/node/node.h \
+ /home/phil/.node-gyp/8.6.0/include/node/node_object_wrap.h \
+ ../node_modules/nan/nan_callbacks.h \
+ ../node_modules/nan/nan_callbacks_12_inl.h \
+ ../node_modules/nan/nan_maybe_43_inl.h \
+ ../node_modules/nan/nan_converters.h \
+ ../node_modules/nan/nan_converters_43_inl.h \
+ ../node_modules/nan/nan_new.h \
+ ../node_modules/nan/nan_implementation_12_inl.h \
+ ../node_modules/nan/nan_persistent_12_inl.h \
+ ../node_modules/nan/nan_weak.h ../node_modules/nan/nan_object_wrap.h \
+ ../node_modules/nan/nan_private.h \
+ ../node_modules/nan/nan_typedarray_contents.h \
+ ../node_modules/nan/nan_json.h
+../hello.cc:
+../node_modules/nan/nan.h:
+/home/phil/.node-gyp/8.6.0/include/node/node_version.h:
+/home/phil/.node-gyp/8.6.0/include/node/uv.h:
+/home/phil/.node-gyp/8.6.0/include/node/uv-errno.h:
+/home/phil/.node-gyp/8.6.0/include/node/uv-version.h:
+/home/phil/.node-gyp/8.6.0/include/node/uv-unix.h:
+/home/phil/.node-gyp/8.6.0/include/node/uv-threadpool.h:
+/home/phil/.node-gyp/8.6.0/include/node/uv-linux.h:
+/home/phil/.node-gyp/8.6.0/include/node/node.h:
+/home/phil/.node-gyp/8.6.0/include/node/v8.h:
+/home/phil/.node-gyp/8.6.0/include/node/v8-version.h:
+/home/phil/.node-gyp/8.6.0/include/node/v8config.h:
+/home/phil/.node-gyp/8.6.0/include/node/node_version.h:
+/home/phil/.node-gyp/8.6.0/include/node/node_buffer.h:
+/home/phil/.node-gyp/8.6.0/include/node/node.h:
+/home/phil/.node-gyp/8.6.0/include/node/node_object_wrap.h:
+../node_modules/nan/nan_callbacks.h:
+../node_modules/nan/nan_callbacks_12_inl.h:
+../node_modules/nan/nan_maybe_43_inl.h:
+../node_modules/nan/nan_converters.h:
+../node_modules/nan/nan_converters_43_inl.h:
+../node_modules/nan/nan_new.h:
+../node_modules/nan/nan_implementation_12_inl.h:
+../node_modules/nan/nan_persistent_12_inl.h:
+../node_modules/nan/nan_weak.h:
+../node_modules/nan/nan_object_wrap.h:
+../node_modules/nan/nan_private.h:
+../node_modules/nan/nan_typedarray_contents.h:
+../node_modules/nan/nan_json.h:
